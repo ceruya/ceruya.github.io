66ece125b5ab834c819c4310178ef7bb70e62433
Ceren <ruya.erden@gmail.com>
2024-01-30 01:00:28 -0500

    design changes
    -fixed the resume link
    -done some basic css updates

--- assets/script.js
+++ assets/script.js
@@ -18,19 +18,3 @@ internalLinks.forEach(link => {
     });
   });
 });
-
-// Select the dark mode toggle button
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-
-// Add click event listener to the toggle button
-darkModeToggle.addEventListener('click', () => {
-  // Toggle the dark mode class on the body element
-  document.body.classList.toggle('dark-mode');
-
-  // Update the toggle button text
-  if (document.body.classList.contains('dark-mode')) {
-    darkModeToggle.textContent = 'Light Mode';
-  } else {
-    darkModeToggle.textContent = 'Dark Mode';
-  }
-});
