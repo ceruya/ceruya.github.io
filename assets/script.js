558c2a574e0e2a73c3aa8cf80345a934618fe4b3
Ceren <ruya.erden@gmail.com>
2024-01-31 17:26:25 -0500

    ---

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,74 @@ internalLinks.forEach(link => {
     // Get the target element and its position
     const targetId = link.getAttribute('href');
     const targetElement = document.querySelector(targetId);
-    const targetPosition = targetElement.getBoundingClientRect().top;
 
-    // Scroll smoothly to the target position
+    // Get the height of the fixed navbar
+    const navbar = document.querySelector('nav');
+    const navbarHeight = navbar.offsetHeight;
+
+    // Scroll smoothly to the target position with consideration for navbar height
     window.scrollTo({
-      top: targetPosition,
+      top: targetElement.offsetTop - navbarHeight + 1, // Adding a small offset
       behavior: 'smooth'
+    });
+  });
+});
+
+// Navbar active 
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+  const navItems = document.querySelectorAll(".nav-link");
+
+  // Function to add or remove the "active" class
+  function toggleActiveClass(targetId) {
+    navItems.forEach((navItem) => {
+      const href = navItem.getAttribute("href").slice(1);
+      navItem.classList.toggle("active", href === targetId);
+    });
+  }
+
+// Event listener for scroll
+let ticking = false;
+
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      let current = "";
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+          current = section.getAttribute("id");
+        }
+      });
+
+      toggleActiveClass(current);
+
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+    toggleActiveClass(current);
+  });
+
+  // Event listener for click on navigation links
+  navItems.forEach((navItem) => {
+    navItem.addEventListener("click", (event) => {
+      event.preventDefault();
+      const targetId = navItem.getAttribute("href").slice(1);
+
+      // Scroll to the target section
+      document.getElementById(targetId).scrollIntoView({
+        behavior: "smooth"
+      });
+
+      // Add or remove the "active" class
+      toggleActiveClass(targetId);
     });
   });
 });
